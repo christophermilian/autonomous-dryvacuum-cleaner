0eba1a304f76c66a5ebeaf85ca3823b000a9d319
Christopher Milian <christophermilian16@gmail.com>
2021-04-02 11:45:31 -0400

    Set up repo with project files

---
+++ build/lidar_bot/src/ros/lidarbot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/catkin_ws/src/lidar_bot/src/ros/lidarbot
+# Build directory: /home/ubuntu/catkin_ws/build/lidar_bot/src/ros/lidarbot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/lidar_bot/src/ros/wall_follower_sim/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/catkin_ws/src/lidar_bot/src/ros/wall_follower_sim
+# Build directory: /home/ubuntu/catkin_ws/build/lidar_bot/src/ros/wall_follower_sim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
