f0971032250b75df178fe435a9b93a51dc477fcd
Christopher Milian <cmilian@getspruce.com>
2022-10-19 14:08:20 -0500

    files: remove outdated project files

--- build/RPlidar_Hector_SLAM/hector_slam/hector_imu_attitude_to_tf/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/catkin_ws/src/RPlidar_Hector_SLAM/hector_slam/hector_imu_attitude_to_tf
-# Build directory: /home/ubuntu/catkin_ws/build/RPlidar_Hector_SLAM/hector_slam/hector_imu_attitude_to_tf
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/wall_follower_sim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/catkin_ws/src/wall_follower_sim
-# Build directory: /home/ubuntu/catkin_ws/build/wall_follower_sim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
